3a5538f4eee2af773e0a3b247270df7e64f7632b
TanLP2003 <phutanle372@gmail.com>
2024-10-16 13:34:15 +0700

    complete calculator

--- app/src/main/java/com/example/caculator/MainActivity.kt
+++ app/src/main/java/com/example/caculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.caculator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var resultOutput: TextView? = null
+    private var lastDigit: Boolean = false
+    private var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
@@ -19,9 +25,143 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         supportActionBar?.title = "LifeCycle"
+
+        resultOutput = findViewById(R.id.resultOutput)
     }
 
-    fun onDigit(view: View) {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+    fun onDigit(view: View){
+        if(resultOutput?.text.toString().length == 1 && resultOutput?.text.toString().startsWith("0")){
+            resultOutput?.text = ""
+        }
+        resultOutput?.append((view as TextView).text)
+        lastDigit = true
+    }
+
+    fun onClear(view: View){
+        resultOutput?.text = "0"
+        lastDigit = true
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastDigit && !lastDot) {
+            resultOutput?.append(".")
+            lastDigit = false 
+            lastDot = true 
+        }
+    }
+    
+    fun onOperator(view: View) {
+        resultOutput?.text?.let {
+            if (lastDigit && !isOperatorAdded(it.toString())) {
+                resultOutput?.append((view as TextView).text)
+                lastDigit = false
+                lastDot = false
+            }
+        }
+    }
+
+    fun onDelete(view: View){
+        var resValue = resultOutput?.text.toString()
+        if(resValue.length <= 1){
+            resultOutput?.text = "0"
+            lastDigit = false
+            return
+        }else {
+            if(resValue.endsWith(".")){
+                lastDot = false
+                lastDigit = true
+            }
+        }
+        resultOutput?.text = resValue.dropLast(1)
+        if(resultOutput?.text.toString().endsWith(".")){
+            lastDot = true
+            lastDigit = false
+        }
+    }
+    
+    fun onEqual(view: View) {
+        if (lastDigit) {
+            var resValue = resultOutput?.text.toString()
+            var prefix = ""
+            try {
+                if (resValue.startsWith("-")) {
+                    prefix = "-"
+                    resValue = resValue.substring(1);
+                }
+                when {
+                    resValue.contains("/") -> {
+                        val splitValue = resValue.split("/")
+
+                        var one = splitValue[0]
+                        val two = splitValue[1]
+
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+
+                        resultOutput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                    }
+                    resValue.contains("x") -> {
+                        val splitValue = resValue.split("x")
+
+                        var one = splitValue[0] // Value One
+                        val two = splitValue[1] // Value Two
+
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+
+                        resultOutput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                    }
+                    resValue.contains("-") -> {
+                        val splitValue = resValue.split("-")
+                        var one = splitValue[0]
+                        val two = splitValue[1] 
+
+                        if (prefix.isNotEmpty()) { 
+                            one = prefix + one
+                        }
+                        
+                        resultOutput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                    }
+                    resValue.contains("+") -> {
+                        val splitValue = resValue.split("+")
+
+                        var one = splitValue[0] 
+                        val two = splitValue[1] 
+
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+
+                        resultOutput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                    }
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String): String {
+        var value = result
+
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+
+        return value
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            (value.contains("/")
+                    || value.contains("*")
+                    || value.contains("-")
+                    || value.contains("+"))
+        }
     }
 }
